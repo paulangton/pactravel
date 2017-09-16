7ad85b74fb7f7a06f94f051e25793b96fb9733a3
Alex Aubuchon <alex@alxe.co>
2017-09-16 17:29:04 -0400

    Got a map up with a path

--- public/app.js
+++ public/app.js
@@ -1,2 +1,39 @@
 "use strict";
+function PixelCoords(map, latLong) {
+    var scale = Math.pow(2, map.getZoom());
+    var nw = new google.maps.LatLng(map.getBounds().getNorthEast().lat(), map.getBounds().getSouthWest().lng());
+    var worldCoordinateNW = map.getProjection().fromLatLngToPoint(nw);
+    var worldCoordinate = map.getProjection().fromLatLngToPoint(latLong);
+    var pixelOffset = new google.maps.Point(Math.floor((worldCoordinate.x - worldCoordinateNW.x) * scale), Math.floor((worldCoordinate.y - worldCoordinateNW.y) * scale));
+    return [0, 0];
+}
 console.log("Hello");
+var map;
+function Initialize() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: { lat: 0, lng: -180 },
+        mapTypeId: 'terrain'
+    });
+    var flightPlanCoordinates = [
+        { lat: 37.772, lng: -122.214 },
+        { lat: 21.291, lng: -157.821 },
+        { lat: -18.142, lng: 178.431 },
+        { lat: -27.467, lng: 153.027 }
+    ];
+    var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    flightPath.setMap(map);
+}
+function Update() {
+}
+function OnFlight(newCity) {
+}
+// 
+function OnCityLoad(newCityGraph) {
+}
---
+++ public/src/GMapUtil.ts
@@ -0,0 +1,15 @@
+
+function PixelCoords(map: any, latLong : any) : number[] {
+    var scale = Math.pow(2, map.getZoom());
+    var nw = new google.maps.LatLng(
+        map.getBounds().getNorthEast().lat(),
+        map.getBounds().getSouthWest().lng()
+    );
+    var worldCoordinateNW = map.getProjection().fromLatLngToPoint(nw);
+    var worldCoordinate = map.getProjection().fromLatLngToPoint(latLong);
+    var pixelOffset = new google.maps.Point(
+        Math.floor((worldCoordinate.x - worldCoordinateNW.x) * scale),
+        Math.floor((worldCoordinate.y - worldCoordinateNW.y) * scale)
+    );
+    return [0,0];
+}
