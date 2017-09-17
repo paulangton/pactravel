cf2c325e9e47493a3be5fac1d140e11ac86ea8f7
Alex Aubuchon <alex@alxe.co>
2017-09-17 09:47:15 -0400

    Style and functionality improvements

--- graph.py
+++ graph.py
@@ -25,7 +25,7 @@ def api_call(latitude=42.3656132, longitude=-71.00956020000001, category="Museum
 
 	# create an instance of the API class
 	api_instance = swagger_client.DefaultApi()
-	apikey = '5ZuuDhbYHXWkNpo7AKhxr6Ceb1aV4z3C' # str | API Key provided for your account, to identify you for API access. Make sure to keep this API key secret.
+	apikey = 'xe3IjAFO8GO5UqpQVe4kpzfjHxl4ERtz' # str | API Key provided for your account, to identify you for API access. Make sure to keep this API key secret.
 	radius = 42 # int | Radius around the center to look for points-of-interest around the given latitude and longitude in kilometers (km) (default to 42)
 	lang = 'EN' # str | The preferred language of the content related to each point of interest. Content will be returned in this language if available (optional) (default to EN)
 	category = 'Museum' # str | Filters the resulting points_of_interest to include only results which have a least one category containing the given provided word. Good examples are <em>museum</em>, <em>landmark</em> or <em>church</em> (optional) (default to Museum)
