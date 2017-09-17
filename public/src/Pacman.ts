c20861afd258853ebf96750e9f0884c898e3f1e0
Alex Aubuchon <alex@alxe.co>
2017-09-16 20:29:19 -0400

    Fixed up import of pacman.ts

--- public/src/Pacman.ts
+++ public/src/Pacman.ts
@@ -1,11 +1,10 @@
-import CityGraph from "./CityGraph";
 
 class Pacman {
   lat : number;
   long : number;
   mouthState : boolean;
-  fromNode : CityGraph.ANode;
-  toNode : CityGraph.ANode;
+  fromNode : ANode;
+  toNode : ANode;
 
   // Reverses pacman's direction
   rev() : void {
