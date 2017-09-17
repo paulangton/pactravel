239bfcfac2c41a4a996b5e3d5b09d7e7f5d5e823
Louis <louis.boguslav312@gmail.com>
2017-09-17 00:30:34 -0400

    next destination

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, jsonify
+from amadeus.next_location import Next_Desitination
+import graph
+
+
 app = Flask(__name__)
+dest = Next_Desitination()
 
-@app.route('/hello')
-def api_hello():
-    if 'name' in request.args:
-        return 'Hello ' + request.args['name']
-    else:
-        return 'Hello John Doe'
+@app.route('/next_locations')
+def next_locations():
+    return jsonify({'dest': dest.getLocations('NYC')})
 
+@app.route('/poi')
+def points_of_interest():
+    return "hello"
+            
 if __name__ == '__main__':
     app.run()
