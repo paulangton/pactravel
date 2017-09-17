0c3ef3744ae4238c0170b5dbe1daa681143fd71a
Louis <louis.boguslav312@gmail.com>
2017-09-17 01:10:13 -0400

    finalized

--- main.py
+++ main.py
@@ -9,21 +9,18 @@ dest = Next_Desitination()
 @app.route('/next_locations')
 def next_locations():
     loc = request.args['location']
-    print(loc)
-    # loc = "NYC"
-    return jsonify({'dest': dest.getLocations(loc)})
+    return jsonify({'data': dest.getLocations(loc)})
 
 @app.route('/poi')
 def points_of_interest():
-	print("YO")
 	lat = int(request.args['latitude'])
 	longit = int(request.args['longitude'])
 	airport_name = request.args['airport_name']
 	
 	if lat == 0 and longit == 0:
-		return jsonify({'flugidablah': graph.genGraph()})
+		return jsonify({'data': graph.genGraph()})
 	else:
-		return jsonify({'flugidablah': graph.genGraph(initial_airport=airport_name, latit=lat, longit=longit)})
+		return jsonify({'data': graph.genGraph(initial_airport=airport_name, latit=lat, longit=longit)})
             
 if __name__ == '__main__':
 	app.run()
